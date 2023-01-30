350fdfe2368bf7e9d822bffd12922c7370806e3d
Leonardo Alabart <leoanrdo.alabart@gmail.com>
2023-01-30 15:01:14 -0300

    Some advance in Favorites ordering and filtering

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -4,19 +4,45 @@ import { connect } from 'react-redux';
 
 export function Favorites(props) {
     //    const { characters } = props;
+
+    const handleOrderChange = (e) => {
+
+    };
+
+    const handleFilterChange = (e) => {
+
+    };
+
     return (
-        <div className='divFavCards'>
-            {/* Only sending some of the data to the Card component to differentiate from the home cards page */}
-            {props.myFavorites.map((elem, index) =>
-                <Card
-                    key={index}
-                    id={elem.id}
-                    name={elem.name}
-                    // species={elem.species}
-                    // gender={elem.gender}
-                    image={elem.image}
-                />)}
-        </div>
+        <>
+            <div className='divOrderFiltering'>
+                <label for='order'>Choose Ordering (asc/desc):</label>
+                <select name='order' id="order" onChange={handleOrderChange}>
+                    <option value='ASC'>Ascending</option>
+                    <option value='DESC'>Descending</option>
+                </select>
+
+                <label for='filter'>Choose Filtering</label>
+                <select name='filter' id='filter' onChange={handleFilterChange}>
+                    <option value='Male'>Male</option>
+                    <option value='Female'>Female</option>
+                    <option value='Genderless'>Genderless</option>
+                    <option value='unknown'>unknown</option>
+                </select>
+            </div>
+            <div className='divFavCards'>
+                {/* Only sending some of the data to the Card component to differentiate from the home cards page */}
+                {props.myFavorites.map((elem, index) =>
+                    <Card
+                        key={index}
+                        id={elem.id}
+                        name={elem.name}
+                        // species={elem.species}
+                        // gender={elem.gender}
+                        image={elem.image}
+                    />)}
+            </div>
+        </>
     )
 };
 
@@ -26,4 +52,4 @@ export function mapStateToProps(state) {
     };
 };
 
-export default connect(mapStateToProps, null) (Favorites);
+export default connect(mapStateToProps, null)(Favorites);
--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,4 +1,4 @@
-import { ADD_FAVORITE, DELETE_FAVORITE } from "../actions/types.js";
+import { ADD_FAVORITE, DELETE_FAVORITE, FILTER } from "../actions/types.js";
 
 const initialState = {
     myFavorites: [],    // Selected favorite characters
@@ -15,11 +15,20 @@ function rootReducer(state = initialState, { type, payload }) {
             }
         case DELETE_FAVORITE:
             const newFarorites = state.myFavorites.filter(
-                favChar => favChar.id !== payload)
+                favChar => favChar.id !== payload);
+            const newAllCharacters = state.allCharacters.filter(
+                favChar => favChar.id !== payload);
             return {
                 ...state,
                 myFavorites: newFarorites,
+                allCharacters: newAllCharacters,
             }
+            case FILTER:
+                const filteredByGender = state.allCharacters.filter(char => char.gender !== payload);
+                return {
+                    ...state,
+                    myFavorites: [...filteredByGender],
+                }
         default:
             return state
     }
