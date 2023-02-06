5075780dfb057cc2ad5c9f143df0ec557f8c957e
Leo <leonardo.alabart@gmail.com>
2023-02-05 21:16:42 -0300

    Favorites component: Added local states to control selects' values. Added styles to selects

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -2,18 +2,24 @@ import './favorites.css';
 import Card from './Card';
 import { connect, useDispatch } from 'react-redux';
 import * as actions from '../redux/actions/index.js';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export function Favorites(props) {
-    //    const { characters } = props;
+    const [selectedOrdering, setSelectedOrdering] = useState("");
+    const [selectedFiltering, setSelectedFiltering] = useState("");
+
     const dispatch = useDispatch();
 
     const handleOrderChange = (e) => {
         dispatch(actions.orderCards(e.target.value));
+        setSelectedOrdering(e.target.value);
+        setSelectedFiltering("");
     };
 
     const handleFilterChange = (e) => {
         dispatch(actions.filterCards(e.target.value));
+        setSelectedFiltering(e.target.value);
+        setSelectedOrdering("");
     };
 
     useEffect(() => {
@@ -22,16 +28,27 @@ export function Favorites(props) {
 
     return (
         <>
-            <div className='divOrderFiltering'>
-                {/* <label htmlFor='order'>Choose Ordering (asc/desc):</label> */}
-                <select name='order' id="order" onChange={handleOrderChange} defaultValue="">
+            {/* Ordering and Filtering buttons */}
+            <div
+                className='divOrderFiltering'>
+                <select className='selectOrderFilter'
+                    name='order'
+                    value={selectedOrdering}
+                    onChange={handleOrderChange}
+                // defaultValue=""
+                >
                     <option value="" disabled>Select Order</option>
                     <option value='ASC'>Ascending</option>
                     <option value='DESC'>Descending</option>
                 </select>
 
-                {/* <label htmlFor='filter'>Choose Filtering</label> */}
-                <select name='filter' id='filter' onChange={handleFilterChange} defaultValue="">
+                <select
+                    className='selectOrderFilter'
+                    name='filter'
+                    value={selectedFiltering}
+                    onChange={handleFilterChange}
+                // defaultValue=""
+                >
                     <option value="" disabled>Select Gender</option>
                     <option value='Male'>Male</option>
                     <option value='Female'>Female</option>
@@ -39,6 +56,8 @@ export function Favorites(props) {
                     <option value='unknown'>unknown</option>
                 </select>
             </div>
+
+            {/* Favorite Cards */}
             <div className='divFavCards'>
                 {props.myFavorites.map((elem, index) =>
                     <Card
