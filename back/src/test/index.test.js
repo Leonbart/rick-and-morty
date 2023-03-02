dbd55f74b887671f05badf80c9d486aac43ebfe0
Leonardo Alabart <leoanrdo.alabart@gmail.com>
2023-03-02 20:23:08 -0300

    Minor changes

--- back/src/test/index.test.js
+++ back/src/test/index.test.js
@@ -3,5 +3,7 @@ const session = require('supertest');
 const agent = session(app);
 
 describe("Test de RUTAS", () => {
-
+    describe('GET rickandmorty/{id}', () => {
+        it('Responde con status: 200', )
+    })
 });
