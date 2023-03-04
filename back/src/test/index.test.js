e07a8ff3893f42a710a105bc97d424079e133152
Leo <leonardo.alabart@gmail.com>
2023-03-03 23:56:14 -0300

    M3-07 homework partially done

--- back/src/test/index.test.js
+++ back/src/test/index.test.js
@@ -1,9 +1,14 @@
-const app = require('../app.js');
+const app = require('../../app.js');
 const session = require('supertest');
 const agent = session(app);
 
 describe("Test de RUTAS", () => {
     describe('GET rickandmorty/{id}', () => {
-        it('Responde con status: 200', )
+        it('Responde con status: 200', () => {
+            agent.get('/rickandmorty/1').expect(200);
+        });
+        it('Responde un objeto con las propiedades: "id", "name", "species", "gender" e "image"', () => {
+            return expect(agent.get('/rickandmorty/1')).resolves.toContain("id", "name", "species", "gender", "image")
+        });
     })
 });
